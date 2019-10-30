f3ed91a905cf3975bb4c578480ad0eaaa8a4213e
Ice-Jeffrey <735429132@qq.com>
2019-10-30 18:10:02 +0800

    photoshop method to adjust contrastness and brightness

--- Project1.cpp
+++ Project1.cpp
@@ -5,8 +5,11 @@
 using namespace cv;
 using std::cout;
 
-const int alpha_slider_max = 100;
-const int beta_slider_max = 100;
+#define CLIP_RANGE(value, min, max)  ( (value) > (max) ? (max) : (((value) < (min)) ? (min) : (value)) )
+#define COLOR_RANGE(value)  CLIP_RANGE(value, 0, 255)
+
+const int alpha_slider_max = 510;
+const int beta_slider_max = 510;
 const int saturation_slider_max = 255;
 int alpha_slider;
 int beta_slider;
@@ -18,14 +21,25 @@ double saturation;
 Mat src;
 Mat dst;
 
-static void on_trackbar_alpha(int, void*) {
-	alpha = alpha_slider * 3.0 / alpha_slider_max;
-	src.convertTo(dst, -1, alpha, beta);
-	imshow("Brightness and Contrastness", dst);
-}
+static void on_trackbar(int, void*) {
+	/**
+	Algorithm of Brightness Contrast transformation
+	The formula is:
+		y = [x - 127.5 * (1 - B)] * k + 127.5 * (1 + B);
+		x is the input pixel value
+		y is the output pixel value
+		B is brightness, value range is [-1,1]
+		k is used to adjust contrast
+			k = tan( (45 + 44 * c) / 180 * PI );
+			c is contrast, value range is [-1,1]
+	*/
 
-static void on_trackbar_beta(int, void*) {
-	beta = beta_slider;
+	double B = (beta_slider - 255) * 1.0 / 255;
+	double c = (alpha_slider - 255) * 1.0 / 255;
+	double k = tan((45 + 44 * c) / 180 * 3.14);
+	double avg = (cv::mean(src)[0] + cv::mean(src)[1] + cv::mean(src)[2]) * 1.0 / 3;
+	alpha = k;
+	beta = avg * (B * k + B - k + 1);
 	src.convertTo(dst, -1, alpha, beta);
 	imshow("Brightness and Contrastness", dst);
 }
@@ -37,9 +51,9 @@ static void HSVadjust(int, void*) {
 	for (int i = 0; i < src.rows; i++) {
 		for (int j = 0; j < src.cols; j++) {
 			//saturation_slider = image_hsv.at<Vec3b>(i, j)[1];
-			image_hsv.at<Vec3b>(i, j)[1] = saturate_cast<uchar>(
+			image_hsv.at<Vec3b>(i, j)[1] = COLOR_RANGE(
 				image_hsv.at<Vec3b>(i, j)[1] + saturation
-				);
+			);
 		}
 	}
 	cvtColor(image_hsv, dst, CV_HSV2BGR);
@@ -48,24 +62,24 @@ static void HSVadjust(int, void*) {
 
 int main(void)
 {
-	src = imread("F:\\Codes\\DigitalMedia2019\\Project_I\\Jerome.jpg");
+	src = imread("C:\\Users\\Jeffrey\\Pictures\\Jerome.jpg");
 	if (src.empty()) { cout << "Error loading source image! \n"; return -1; }
 
 	namedWindow("Brightness and Contrastness", WINDOW_AUTOSIZE); // Create Window
-	
-	//设定对比度初始值为50
-	alpha_slider = 50;
+
+	//设定对比度初始值为255
+	alpha_slider = 255;
 	char alphaName[50];
 	sprintf_s(alphaName, "对比度 %d", alpha_slider_max);
-	createTrackbar(alphaName, "Brightness and Contrastness", &alpha_slider, alpha_slider_max, on_trackbar_alpha);
-	on_trackbar_alpha(alpha_slider, 0);
+	createTrackbar(alphaName, "Brightness and Contrastness", &alpha_slider, alpha_slider_max, on_trackbar);
+	on_trackbar(alpha_slider, 0);
 
-	//设定亮度初始值为50
-	beta_slider = 50;
+	//设定亮度初始值为255
+	beta_slider = 255;
 	char betaName[50];
 	sprintf_s(betaName, "亮度 %d", beta_slider_max);
-	createTrackbar(betaName, "Brightness and Contrastness", &beta_slider, beta_slider_max, on_trackbar_beta);
-	on_trackbar_beta(beta_slider, 0);
+	createTrackbar(betaName, "Brightness and Contrastness", &beta_slider, beta_slider_max, on_trackbar);
+	on_trackbar(beta_slider, 0);
 
 	//设定饱和度初始值为50
 	saturation_slider = 0;
@@ -73,7 +87,7 @@ int main(void)
 	sprintf_s(satName, "饱和度 %d", saturation_slider_max);
 	createTrackbar(satName, "Brightness and Contrastness", &saturation_slider, saturation_slider_max, HSVadjust);
 	HSVadjust(saturation_slider, 0);
-	
+
 
 	waitKey(0);
 	return 0;
